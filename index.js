7429aa336e292620e43f58cfe6aee54fe8b9172d
Your Name <your_email@example.com>
2023-10-07 12:38:17 -0300

    meu terceiro commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("olá mundo!");
+var nome = "Giovanni";
+var xp = 1000;
+var nivel;
+
+if(xp < 1000) {
+    console.log("Seu rank é ferro");
+    nivel = "ferro";
+    console.log("O herói de nome " + nome + " está no nível de " + nivel);
+} else if(xp < 2001) {
+    console.log("Seu rank é bronze");
+    nivel = "bronze";
+    console.log("O herói de nome " + nome + " está no nível de " + nivel);
+} else if(xp < 6001) {
+    console.log("Seu rank é prata");
+    nivel = "prata";
+    console.log("O herói de nome " + nome + " está no nível de " + nivel);
+} else if(xp < 7001) {
+    console.log("Seu rank é ouro");
+    nivel = "ouro";
+    console.log("O herói de nome " + nome + " está no nível de " + nivel);
+} else if(xp < 8001) {
+    console.log("Seu rank é platina");
+    nivel = "platina";
+    console.log("O herói de nome " + nome + " está no nível de " + nivel);
+} else if(xp < 9001) {
+    console.log("Seu rank é ascendente");
+    nivel = "ascendente";
+    console.log("O herói de nome " + nome + " está no nível de " + nivel);
+} else if(xp >= 9001) {
+    console.log("Seu rank é radiante");
+    nivel = "radiante";
+    console.log("O herói de nome " + nome + " está no nível de " + nivel);
+}
+
+
